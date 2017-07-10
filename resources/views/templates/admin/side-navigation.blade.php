bf99a422c99199c82d3bdfba59c1d7aae3b8a604
Mark Armstrong <armstrongmark14@gmail.com>
2017-07-09 23:29:33 -0400

    Updated admin side navigation

--- resources/views/templates/admin/side-navigation.blade.php
+++ resources/views/templates/admin/side-navigation.blade.php
@@ -10,12 +10,18 @@
                 <li><a href="{{ route('admin-create-volunteer') }}"><i class="fa fa-plus-square"></i> Add Volunteer</a></li>
                 <li>&nbsp;</li>
                 <li><a href="{{ route('total-hours-chart') }}"><i class="fa fa-bar-chart"></i> Charts</a></li>
+
+                @if(Auth::user() && Auth::user()->isSuperAdmin()))
+                    
                 <li>&nbsp;</li>
                 <li><a href="{{ route('super-admin-dashboard') }}"><i class="fa fa-dashboard"></i> Admin Dashboard</a></li>
-                <li>&nbsp;</li>
                 <li><a href="{{ route('admin-add-user') }}"><i class="fa fa-user-plus"></i> Add User</a></li>
                 <li><a href="{{ route('manage-users') }}"><i class="fa fa-wrench"></i> Manage Users</a></li>
-                <li><a href="{{ route('admin-change-password') }}"><i class="fa fa-unlock"></i> Change Password</a></li>
+       
+                @endif
+              
+               <li>&nbsp;</li>
+               <li><a href="{{ route('admin-change-password') }}"><i class="fa fa-unlock"></i> Change Password</a></li>
             </ul>
         </div>
     </div>
